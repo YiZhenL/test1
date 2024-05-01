4dfe89239b163c6a856db5298c9eaef27b1148d7
苡溱 廖 <5b0g0904@office.stust.edu.tw>
2024-05-01 13:54:48 +0800

    test

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          五專資工三甲 5B0G0904 廖苡溱
         </p>
         <a
           className="App-link"
