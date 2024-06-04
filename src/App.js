899f6f599f86790e8613f8a54ed6826e52bb1169
苡溱 廖 <5b0g0904@office.stust.edu.tw>
2024-06-04 11:53:50 +0800

    Slider

--- src/App.js
+++ src/App.js
@@ -1,28 +1,21 @@
+import './App.css';
+import MyRGBPanel from './rgbpanel/MyRGBPanel';
+import MySlider from './rgbpanel/MySlider';
+import MyCalculator from './components/MyCalculator';
 
-import "./App.css";
-import MySlider from "./MySlider";
 
 function App() {
   return (
     <div className="App">
-      <h1>Javascript程式設計</h1>
-      <h2>5B0G0904 廖苡溱</h2>
-      <span>R: </span><MySlider />
-      <span>G: </span><MySlider />
-      <span>B: </span><MySlider />
-      {/* <header className="App-header"> */}
-      {/* <img src={logo} className="App-logo" alt="logo" />
-        <h1>Hello React! 2024.5.1</h1>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a> */}
-
-      {/* </header> */}
+      <header className="App-header">
+        <h3>5B0G0904 廖苡溱</h3>
+        {/* <h3>MySlider</h3>
+        <MySlider/> */}
+        <h3>MyRGBPanel</h3>
+        <MyRGBPanel/>
+        <h3>MyCalculator</h3>
+        <MyCalculator/>
+      </header>
     </div>
   );
 }
